3468128fcdcdd60a319101d06c485c7fd46a6551
nat0t <nataniel1987@gmail.com>
2021-04-08 07:35:30 +0300

    Fixed OrderUpdate.

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -20,13 +20,14 @@ class OrderList(LoginRequiredMixin, ListView):
         return Order.objects.filter(user=self.request.user)
 
 
-class OrderItemCreate(LoginRequiredMixin, CreateView):
+class OrderCreate(LoginRequiredMixin, CreateView):
     model = Order
     fields = []
+    context_object_name = 'object'
     success_url = reverse_lazy('ordersapp:orders_list')
 
     def get_context_data(self, **kwargs):
-        data = super(OrderItemCreate, self).get_context_data(**kwargs)
+        data = super(OrderCreate, self).get_context_data(**kwargs)
         OrderFormSet = inlineformset_factory(Order, OrderItem,
                                              form=OrderItemForm, extra=1)
         if self.request.POST:
@@ -61,40 +62,23 @@ class OrderItemCreate(LoginRequiredMixin, CreateView):
         if self.object.get_total_cost() == 0:
             self.object.delete()
 
-        return super(OrderItemCreate, self).form_valid(form)
+        return super(OrderCreate, self).form_valid(form)
 
 
-class OrderItemUpdate(LoginRequiredMixin, UpdateView):
+class OrderUpdate(LoginRequiredMixin, UpdateView):
     model = Order
     fields = []
     success_url = reverse_lazy('ordersapp:orders_list')
 
     def get_context_data(self, **kwargs):
-        data = super(OrderItemUpdate, self).get_context_data(**kwargs)
+        data = super(OrderUpdate, self).get_context_data(**kwargs)
         OrderFormSet = inlineformset_factory(Order, OrderItem,
                                              form=OrderItemForm, extra=1)
         if self.request.POST:
-            data['orderitems'] = OrderFormSet(self.request.POST,
-                                              instance=self.object)
+            formset = OrderFormSet(self.request.POST, instance=self.object)
         else:
-            data['orderitems'] = OrderFormSet(instance=self.object)
-
-        # if self.request.POST:
-        #     formset = OrderFormSet(self.request.POST)
-        # else:
-        #     basket_items = Basket.objects.filter(user=self.request.user)
-        #     if len(basket_items):
-        #         OrderFormSet = inlineformset_factory(Order, OrderItem,
-        #                                              form=OrderItemForm,
-        #                                              extra=len(basket_items))
-        #         formset = OrderFormSet()
-        #         for num, form in enumerate(formset.forms):
-        #             form.initial['product'] = basket_items[num].product
-        #             form.initial['quantity'] = basket_items[num].quantity
-        #         basket_items.delete()
-        #     else:
-        #         formset = OrderFormSet()
-        # data['orderitems'] = formset
+            formset = OrderFormSet(instance=self.object)
+        data['orderitems'] = formset
         return data
 
     def form_valid(self, form):
@@ -111,7 +95,7 @@ class OrderItemUpdate(LoginRequiredMixin, UpdateView):
         if self.object.get_total_cost() == 0:
             self.object.delete()
 
-        return super(OrderItemCreate, self).form_valid(form)
+        return super(OrderUpdate, self).form_valid(form)
 
 
 class OrderDelete(DeleteView):
